60b8075dd4e2e6bdb85938bb2ec13f41b2504397
Nacturne <xliangs1122@gmail.com>
2016-01-14 23:19:07 -0500

    python_tools/Commands/simplify.py modified

--- python_tools/Commands/simplify.py
+++ python_tools/Commands/simplify.py
@@ -11,8 +11,8 @@ def sentence_simplifier(args):
     print(tree.simplify())
 
 def file_simplifier(args):
-    file_in = open(config.ROOT_DIR + args.I, 'r')
-    file_out = open(config.ROOT_DIR + args.O, 'w')
+    file_in = open(config.ROOT_DIR + '/' + args.I, 'r')
+    file_out = open(config.ROOT_DIR + '/' + args.O, 'w')
     for s in file_in:
         tree = TreeClass.ScoreTree(s)
         file_out.write(tree.simplify() + '\n')
@@ -27,8 +27,8 @@ parser_sentence.add_argument('S', metavar='Sentence', type=str, help='A sentence
 parser_sentence.set_defaults(func=sentence_simplifier)
 
 parser_file = subparsers.add_parser('file', help='Specified if the input is a file')
-parser_file.add_argument('I', metavar='input_path', type=str, help="Please start it with '/', which means root dir")
-parser_file.add_argument('O', metavar='output_path', type=str, help="Please start it with '/', which means root dir")
+parser_file.add_argument('I', metavar='input_path', type=str, help="Path of input file")
+parser_file.add_argument('O', metavar='output_path', type=str, help="Path of output file")
 parser_file.set_defaults(func=file_simplifier)
 
 args = parser.parse_args()
